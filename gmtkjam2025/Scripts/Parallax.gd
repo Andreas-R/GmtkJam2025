de429d97b601d24090b798344e08812ed4a2e48f
Andreas Roschal <andreas.roschal.jr@chello.at>
2025-08-01 12:08:25 +0200

    animated earth

---
+++ gmtkjam2025/Scripts/Parallax.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+@export var scroll_speed: float = 100.0
+@export var parallax_width: int = 512
+
+func _process(delta):
+    for node: Node2D in get_children():
+        node.position.x += scroll_speed * delta
+
+        if scroll_speed > 0:
+            if node.position.x >= parallax_width:
+                node.position.x -= parallax_width
+        else:
+            if node.position.x <= parallax_width:
+                node.position.x += parallax_width
