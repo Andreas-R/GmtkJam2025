255eacd73ed828e433222400a406138a02e295ca
Markus <3749375+moaiaom@users.noreply.github.com>
2025-08-01 13:45:44 +0200

    Add basic Orbit Manager
    Provides function to get the closest orbit to a given V2

--- gmtkjam2025/Scripts/orbit.gd
+++ gmtkjam2025/Scripts/orbit.gd
@@ -1,5 +1,6 @@
 @tool
 extends Node2D
+class_name Orbit
 
 enum OrbitState {
 	IDLE,
@@ -16,8 +17,11 @@ enum OrbitState {
 @export var clockwise_rotation: bool = true
 @export_range(0, 360) var rotation_speed_deg: int = 10
 @export var base_color: Color = Color.WHITE
+@export var hover_color: Color = Color.LIGHT_GRAY # TODO: Not yet in use
 @export var drag_color: Color = Color.GRAY
 @export var target_color: Color = Color.LIGHT_SALMON
+
+var _collider_width
 
 var _state: OrbitState
 var _local_rotation_speed_deg: float
@@ -25,11 +29,13 @@ var _last_mouse_angle: float
 var _color: Color = base_color
 
 func _ready():
+	assert(_collider_width > 0)
 	rotation = 0
 	_state = OrbitState.IDLE
 	_color = base_color
 	_local_rotation_speed_deg = _get_base_rotation_speed()
 	_donut_collider.radius = radius
+	_donut_collider.width = _collider_width
 
 func _draw():
 	assert(parts > 0)
@@ -67,16 +73,28 @@ func _on_input_event(_viewport: Viewport, event: InputEvent, _shape_idx: int):
 			_change_state(OrbitState.DRAGGED)
 			_last_mouse_angle = get_local_mouse_position().rotated(-rotation).angle()
 
-func _change_state(state: OrbitState):
-	var color_tween = get_tree().create_tween()
+func _change_state(state: OrbitState) -> void:
 	match state:
 		OrbitState.IDLE:
-			color_tween.tween_property(self, "_color", base_color, 0.1)
+			get_tree().create_tween().tween_property(self, "_color", base_color, 0.1)
 		OrbitState.DRAGGED:
-			color_tween.tween_property(self, "_color", drag_color, 0.1)
+			get_tree().create_tween().tween_property(self, "_color", drag_color, 0.1)
+		OrbitState.TARGETTED:
+			get_tree().create_tween().tween_property(self, "_color", target_color, 0.1)
 	_state = state
 
 func _get_configuration_warnings() -> PackedStringArray:
 	if not get_children().any(func(c): return is_instance_of(c, DonutCollisionPolygon2D)):
 		return ["Please Attach a DonutCollisionPolygon2D called DonutCollider"]
 	return []
+
+func set_collider_width(collider_width: float) -> void:
+	_collider_width = collider_width
+
+func target() -> void:
+	if _state != OrbitState.DRAGGED:
+		_change_state(OrbitState.TARGETTED)
+
+func untarget() -> void:
+	if _state != OrbitState.DRAGGED:
+		_change_state(OrbitState.IDLE)
