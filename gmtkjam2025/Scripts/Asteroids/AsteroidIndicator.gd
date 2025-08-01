1bcbb67db0b816e6d016a5e706abb3dd4918454b
Andreas Roschal <andreas.roschal.jr@chello.at>
2025-08-01 17:29:18 +0200

    small change

--- gmtkjam2025/Scripts/Asteroids/AsteroidIndicator.gd
+++ gmtkjam2025/Scripts/Asteroids/AsteroidIndicator.gd
@@ -19,9 +19,9 @@ func spawn():
     spawn_tween.tween_property(indicator, "scale", Vector2(indicator.scale.x, 60), 4.0)
     spawn_tween.tween_property(indicator, "modulate", Color(1.0, 0.0, 0.0, 1.0), 0.5).set_delay(4.0)
     spawn_tween.tween_property(indicator, "scale", Vector2(indicator.scale.x, 5), 0.75).set_delay(4.75)
-    spawn_tween.tween_property(asteroid, "position", Vector2(-asteroid.position.x, asteroid.position.y), 4.0).set_delay(4.0)
-    spawn_tween.tween_property(indicator, "scale", Vector2(indicator.scale.x, 0), 0.5).set_delay(7.0)
-    spawn_tween.tween_callback(cleanup).set_delay(8.0)
+    spawn_tween.tween_property(asteroid, "position", Vector2(-asteroid.position.x, asteroid.position.y), 6.0).set_delay(4.0)
+    spawn_tween.tween_property(indicator, "scale", Vector2(indicator.scale.x, 0), 0.5).set_delay(9.0)
+    spawn_tween.tween_callback(cleanup).set_delay(10.0)
 
 func cleanup():
     queue_free()
